5fea95495f670044a33ac845acaa97c99fc6f447
ando <leo81@outlook.jp>
2023-03-20 13:46:07 +0900

    add initila project.

---
+++ IOS16NavigationDemo/IOS16NavigationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS16NavigationDemoApp.swift
+//  IOS16NavigationDemo
+//
+//  Created by LeoAndo on 2023/03/20.
+//
+
+import SwiftUI
+
+@main
+struct IOS16NavigationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
